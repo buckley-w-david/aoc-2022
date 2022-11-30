4f3ba3293c2baf6c9ab4401dd589a2bf92e3a6b9
David Buckley <david@davidbuckley.ca>
2022-11-30 17:45:22 -0500

    init days

---
+++ day22/day22.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Clear the terminal
+print("\033[2J\033[H") # ]]
+
+# Grid, Direction
+# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
+# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
+# for item in g => iterate over items in row major order
+# g.row_major(_with_index)() => iterate over items in row major order
+# g.column_major(_with_index)() => iterate over items in column major order
+# g.apply(func) => call func with each item
+# g.map(func) => return new Grid with results of func
+# g.ray_from((y, x), direction), yields items from a starting point in a direction
+# g.around(_with_index) => What it sounds like
+
+# Graph
+# g = Graph()
+# g.add_edge(from, to, weight=something)
+# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
+
+# ShuntingYard
+# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
+from aoc_utils import * # type: ignore
+
+from aocd import get_data
+
+
+data = get_data(year=2022, day=22, block=True)
+print(data)
+
+# submit(answer, part="a", day=22, year=2022)
