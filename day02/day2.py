835e8e7c1ce5885c82b039768d7072fdd4eac4d5
David Buckley <david@davidbuckley.ca>
2022-12-02 00:15:46 -0500

    add day 2 solution

--- day02/day2.py
+++ day02/day2.py
@@ -1,30 +1,66 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-from aoc_utils import * # type: ignore
-
 from aocd import get_data
 
 
 data = get_data(year=2022, day=2, block=True)
-print(data)
+lines = data.splitlines()
+
+ROCKA = "A"
+PAPERA = "B"
+SCIA = "C"
+
+ROCKB = LOSE = "X"
+PAPERB = TIE = "Y"
+SCIB = WIN = "Z"
+
+rules = {
+    ROCKA: {
+        ROCKB: 4,
+        PAPERB: 8,
+        SCIB: 3,
+    },
+    PAPERA: {
+        ROCKB: 1,
+        PAPERB: 5,
+        SCIB: 9,
+    },
+    SCIA: {
+        ROCKB: 7,
+        PAPERB: 2,
+        SCIB: 6,
+    }
+}
+
+total = 0
+
+for line in lines:
+    a, b = line.split()
+    if a == ROCKA:
+        if b == LOSE:
+            choice = SCIB
+        elif b == TIE:
+            choice = ROCKB
+        elif b == WIN:
+            choice = PAPERB
+    elif a == PAPERA:
+        if b == LOSE:
+            choice = ROCKB
+        elif b == TIE:
+            choice = PAPERB
+        elif b == WIN:
+            choice = SCIB
+    elif a == SCIA:
+        if b == LOSE:
+            choice = PAPERB
+        elif b == TIE:
+            choice = SCIB
+        elif b == WIN:
+            choice = ROCKB
+    total += rules[a][choice]
+    
+print(total)
+    
+
 
 # submit(answer, part="a", day=2, year=2022)
