45b3b61f36ba296bf27a6fcb93a651a87b52979d
David Buckley <david@davidbuckley.ca>
2022-12-03 00:11:39 -0500

    add day 3 solution

--- day03/day3.py
+++ day03/day3.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
 from aoc_utils import * # type: ignore
 
 from aocd import get_data
 
-
 data = get_data(year=2022, day=3, block=True)
-print(data)
+lines = data.splitlines()
 
-# submit(answer, part="a", day=3, year=2022)
+t = 0
+
+for i in range(0, len(lines), 3):
+    a, b, c = lines[i], lines[i+1], lines[i+2]
+    sa, sb, sc = set(a), set(b), set(c)
+
+    common = list(sa.intersection(b).intersection(sc))[0]
+    if common.islower():
+        prior = ord(common) - ord('a') + 1
+    else:
+        prior = ord(common) - ord('A') + 27
+    t += prior
+
+print(t)
