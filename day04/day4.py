f5c6e2d599fcf1d5cd692b2446006eb0aeb7a318
David Buckley <david@davidbuckley.ca>
2022-12-04 00:08:48 -0500

    add day 4 solution

--- day04/day4.py
+++ day04/day4.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
 from aoc_utils import * # type: ignore
 
 from aocd import get_data
 
 
 data = get_data(year=2022, day=4, block=True)
-print(data)
+lines = data.splitlines()
 
-# submit(answer, part="a", day=4, year=2022)
+t = 0
+
+for line in lines:
+    f, s = line.split(",")
+    f_start, f_end = f.split("-")
+    s_start, s_end = s.split("-")
+
+    fs = set(range(int(f_start), int(f_end)+1))
+    ss = set(range(int(s_start), int(s_end)+1))
+
+    if fs.intersection(ss):
+        t += 1
+
+print(t)
