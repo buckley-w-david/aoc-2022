6449f0b7606c6dd28db7866e582403e086e5e3f2
David Buckley <david@davidbuckley.ca>
2022-12-06 00:06:31 -0500

    add day 6 solution

--- day06/day6.py
+++ day06/day6.py
@@ -1,30 +1,11 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-from aoc_utils import * # type: ignore
-
 from aocd import get_data
 
-
 data = get_data(year=2022, day=6, block=True)
-print(data)
 
-# submit(answer, part="a", day=6, year=2022)
+for r in range(0, len(data)-14):
+    packet = data[r:r+14]
+    if len(set(packet)) == 14:
+        print(r+14)
+        break
