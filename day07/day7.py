339993622cba865bfa3bdcafda4a55cedcbafb6d
David Buckley <david@davidbuckley.ca>
2022-12-07 00:41:26 -0500

    add day 7 solution

--- day07/day7.py
+++ day07/day7.py
@@ -1,30 +1,75 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
 from aoc_utils import * # type: ignore
 
+import itertools
 from aocd import get_data
 
 
 data = get_data(year=2022, day=7, block=True)
-print(data)
+lines = data.splitlines()
+g = data.split("\n\n")
+il = map(int, lines)
+ill = map(ints, lines)
 
-# submit(answer, part="a", day=7, year=2022)
+t = 0
+
+filesystem =  { "/": {}}
+from pathlib import Path
+path = Path("/") 
+
+import re
+
+import shlex
+
+state = 0
+for line in lines:
+    m = re.match(r"\$ (.*)", line)
+    if m:
+        state = 0
+        command = m.group(1)
+        parts = shlex.split(command)
+        if parts[0] == "cd":
+            if parts[1] == "..":
+                path = path.parent
+            elif parts[1] == "/":
+                path = Path("/")
+            else:
+                path = path / parts[1]
+        elif parts[0] == "ls":
+            state = 1
+    elif state == 1:
+        m = re.match(r"(\d+) (.*)", line)
+        if m:
+            size, name = m.groups()
+            d = filesystem
+            for p in path.parts:
+                d = d[p]
+            d[name] = int(size)
+        m = re.match(r"dir (.*)", line)
+        if m:
+            name = m.group(1)
+            d = filesystem
+            for p in path.parts:
+                d = d[p]
+            d[name] = {}
+
+sizes = []
+def calc_size(d, name) -> int:
+    t = 0
+    for k, v in d.items():
+        if isinstance(v, int):
+            t += v
+        else:
+            t += calc_size(v, k)
+    sizes.append((name, t)) # gross
+    return t
+
+used = calc_size(filesystem["/"], "/")
+total = 70000000
+target = 30000000
+
+unused = total - used
+diff = target - unused
+
+print(min([s for n, s in sizes if s >= diff]))
